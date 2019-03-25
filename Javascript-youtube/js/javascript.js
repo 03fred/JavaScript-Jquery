6dd80b4e36d850f89dbafbbeb435917162f580ef
fred <fred.rocke02@gmail.com>
2019-03-24 23:09:11 -0300

    Primeira aula

--- Javascript-youtube/js/javascript.js
+++ Javascript-youtube/js/javascript.js
@@ -1,1 +1,24 @@
 console.log('programador a bordo');
+var nome = 'Fred';
+console.log(nome);
+var idade = 25;
+var total = idade + 10;
+console.log(total);
+var total = 50;
+console.log(total);
+
+var nome ='programador a bordo';
+console.log(nome.length);
+console.log('programador'.length);
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
+
+console.log(nome.indexOf('dor'));
+console.log(nome.replace('programador','desenvolvedor'));
+
+console.log(nome.slice(3, 8));
+var canalYoutube = "    programador a bordo";
+console.log(canalYoutube);
+console.log(canalYoutube.trim());
+console.log(`o nome do canal é  ${nome.trim()} foi criado por fulano`)
+//minuto 13
